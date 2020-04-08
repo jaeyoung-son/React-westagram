8a5fae542a3914ef3f6b0d58fe737f802a1aa2bb
손재영 <zkqkzl2@gmailcom>
2020-04-08 19:57:35 +0900

    테스트

--- src/Component/NavMain.js
+++ src/Component/NavMain.js
@@ -1,38 +1,49 @@
-import React, { Component } from 'react';
-import LogoText from '../images/LogoText.png'
-import { withRouter } from 'react-router-dom';
-import './NavMain.scss'
+import React, { Component } from "react";
+import LogoText from "../images/LogoText.png";
+import { withRouter } from "react-router-dom";
+import "./NavMain.scss";
 
 class NavMain extends Component {
+  goToLogin() {
+    this.props.history.push("/");
+  }
 
-    goToLogin () {
-        this.props.history.push('/')
-    }
-
-    render () {
-        return (
-        <nav>
+  render() {
+    console.log("하이");
+    return (
+      <nav>
         <div className="nav_main">
-            <div className="left_main">
-                <img className="camera" src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/logo.png" alt="."/>
-                <img className="logo" src={LogoText} alt="."/>
-            </div>
-            <div className="center_main">
-                <input className="top" type="text" placeholder="검색"/>
-            </div>
-                <div className="right_main">
-                <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png" alt="."/>
-                <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png" alt="."/>
-                <img 
-                    className="click" 
-                    onClick={this.goToLogin.bind(this)} 
-                    src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png" 
-                    alt="."/>
-            </div>
+          <div className="left_main">
+            <img
+              className="camera"
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/logo.png"
+              alt="."
+            />
+            <img className="logo" src={LogoText} alt="." />
+          </div>
+          <div className="center_main">
+            <input className="top" type="text" placeholder="검색" />
+          </div>
+          <div className="right_main">
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+              alt="."
+            />
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+              alt="."
+            />
+            <img
+              className="click"
+              onClick={this.goToLogin.bind(this)}
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+              alt="."
+            />
+          </div>
         </div>
-        </nav>
-    )
-}
+      </nav>
+    );
+  }
 }
 
-export default withRouter(NavMain); 
+export default withRouter(NavMain);
